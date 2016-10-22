f4e349d72e86266ff58ab2a08963930daa1d8294
hut <hut@lepus.uberspace.de>
2016-10-22 17:27:57 +0200

    implemented simple model

--- src/model/AppModel.java
+++ src/model/AppModel.java
@@ -1,5 +1,38 @@
 package model;
 
-public class AppModel {
+import java.util.ArrayList;
+import java.util.List;
 
+public class AppModel {
+	List<Integer> responses, choices;
+	Integer scoreEnemy, scoreMine;
+	boolean I_start;
+	
+	public AppModel() {
+		responses = new ArrayList<Integer>();
+		choices = new ArrayList<Integer>();
+	}
+
+	public void newGame(boolean I_start) {
+		scoreMine = 0;
+		scoreEnemy = 0;
+		responses.clear();
+		this.I_start = I_start;
+	}
+
+	public void enemyResponse(Integer response) {
+		responses.add(response);
+	}
+
+	public Integer makeChoice() {
+		Integer choice;
+		if (responses.size() == 0 && I_start) {
+			choice = 1;
+		} else {
+			Integer lastResponse = responses.get(responses.size() - 1);
+			choice = lastResponse;
+		}
+		choices.add(choice);
+		return choice;
+	}
 }
